a0864bc26a6761cb6c66a64fc79a2b1fbaaa62a3
Martin Simons <martin.simons@simulogics.net>
2015-07-13 12:24:33 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Collmex MOSS Report Generator
+
+A script that generates a valid MOSS (Mini-One-Stop-Shop) report from a Collmex record export file.
+
+## Disclaimer
+
+This script comes with no warranties whatsoever (see licence). It only works with a certain input file format 
+(files exported from the [Collmex accounting software](https://collmex.de/)) and exports to the format
+required by the German *Bundeszentralamt für Steuern* for filing MOSS tax declarations.
+
+## How to use
+
+To run the script, you need to have a working installation of PHP.
+
+Also, in Collmex, you need to use the accounting system they recommend for MOSS-transcations. You can find more details 
+on this (in German) here: [Ausländische Erlösarten](https://collmex.de/cgi-bin/cgi.exe?1005,1,help,auslaendische_erloesarten).
+The account numbers, country codes, tax rates and validity dates of the tax rates are stored in the tab-seprated file 
+```accounts.tsv```. Modify this file to adjust it your accounting scheme. The validity date is currently ignored by the
+script.
+
+Run it like so:
+
+```
+report.php -f input.csv
+```
+
+```input.csv``` refers to a file as exported from Collmex.
+
+Optional parameters are:
+
+```
+-q      Specify the fiscal quarter for which to generate the report. 
+        Defaults to empty (generate report for all input data).
+-o      Output file name. Defaults to report.csv in the script folder.
+```
+
---
+++ report.php
@@ -0,0 +1,168 @@
+<?php
+$use_text = 'Use: ' . basename(__FILE__) . ' -f "filename" -q quarter [-o output_filename]' . PHP_EOL;
+
+$options = getopt('f:q:o:');
+if ($options === FALSE) {
+    die($use_text);
+}
+
+// Get file name.
+if (!isset($options['f']))
+    die($use_text);
+$filename = $options['f'];
+
+// Check whether file exists.
+if (!file_exists($filename))
+    die("File '$filename' does not exist.\n");
+
+// Get quarter (if not set, report will be generated for all data).
+$quarter = null;
+if (isset($options['q'])) {
+    $quarter = intval($options['q']);
+    if ($quarter < 1 || $quarter > 4)
+        die("Invalid value for quarter: $quarter\n");
+}
+
+$output_filename = dirname(__FILE__) . '/report.csv';
+if (isset($options['o'])) {
+    $output_filename = $options['o'];
+}
+
+// Load accounts.
+$accounts = array();
+if (($handle = fopen(dirname(__FILE__) . '/accounts.tsv', 'r')) !== FALSE) {
+    while (($row = fgetcsv($handle, 0, "\t")) !== FALSE) {
+        $account = new stdClass();
+        $account->number = $row[2];
+        $account->country_code = $row[1];
+        $account->tax_percentage = $row[3];
+        $accounts[$account->number] = $account;
+    }
+    fclose($handle);
+} else {
+    die("Failed to load accounts file.\n");
+}
+
+// The Collmex export holds numbers in German format (comma instead of period as decimal separator).
+function parse_number($value)
+{
+    return floatval(str_replace(',', '.', $value));
+}
+
+// Prepare results array.
+$results = array();
+foreach ($accounts as $account) {
+    $res = new stdClass();
+    $res->country_code = $account->country_code;
+    $res->tax_rate = $account->tax_percentage * 100;
+    $res->base = 0;
+    $res->tax = 0;
+    $results[$account->number] = $res;
+}
+
+// Open and parse file.
+if (($handle = fopen($filename, 'r')) !== FALSE) {
+    $indexes = null;
+    $i = 0;
+    $record_id = null;
+    $current_account = null;
+    $current_amount = null;
+    while (($row = fgetcsv($handle, 0, ';')) !== FALSE) {
+        // Use first row to prepare row indexes.
+        if ($i === 0) {
+            $indexes = array();
+            $num = count($row);
+            for ($c = 0; $c < $num; $c++) {
+                $indexes[$row[$c]] = $c;
+            }
+        } else {
+            $r = $row[$indexes['BelegNr']];
+
+            // Reset state whenever new record starts.
+            if ($r !== $record_id) {
+                if ($current_account !== null)
+                    echo "Record $record_id: No matching tax line for account $current_account.\n";
+
+                $record_id = $r;
+                $current_account = null;
+                $current_amount = null;
+            }
+
+            // Check whether record belongs to the selected quarter.
+            if ($quarter !== null) {
+                $date = date_parse($row[$indexes['Belegdatum']]);
+                $q = ceil($date['month'] / 3);
+                if ($q != $quarter)
+                    continue;
+            }
+
+            // If previous line was a relevant account, search for matching tax line.
+            $account_number = $row[$indexes['Konto']];
+            if ($current_account !== null && $account_number == '3818') {
+                // Check whether tax amount corresponds to tax level from account list.
+                $account = $accounts[$current_account];
+                $parsed_tax = parse_number($row[$indexes['Betrag']]);
+                $calculated_tax = round($current_amount * $account->tax_percentage, 2);
+                $diff = round(($parsed_tax - $calculated_tax) * 100);
+                if ($diff > 1) {
+                    // Differences up to 1 cent are seen as acceptable rounding errors.
+                    echo "Record $record_id: Parsed ($parsed_tax) and calculated ($calculated_tax) tax amounts differ by $diff cents.\n";
+                } else {
+                    $result = $results[$current_account];
+                    $result->base += $current_amount;
+                    $result->tax += $parsed_tax;
+                }
+
+                $current_account = null;
+                $current_amount = null;
+            } // Otherwise, record is only relevant when there are MOSS accounts involved.
+            else if ($current_account === null && array_key_exists($account_number, $accounts)) {
+                $current_account = $account_number;
+                $current_amount = parse_number($row[$indexes['Betrag']]);
+            }
+        }
+
+        $i++;
+    }
+    fclose($handle);
+} else {
+    die("Failed to open filename '$filename'.\n");
+}
+
+// Prepare output.
+$output = array();
+$output[] = array(
+    "Land des Verbrauchs",
+    "Umsatzsteuertyp",
+    "Umsatzsteuersatz",
+    "Steuerbemessungs-Grundlage",
+    "Umsatzsteuerbetrag"
+);
+foreach ($results as $result) {
+    if ($result->base == 0)
+        continue;
+
+    $output[] = array(
+        $result->country_code,
+        "STANDARD",
+        number_format($result->tax_rate, 2),
+        number_format($result->base * -1, 2, '.', ''), // Credit values are negative in Collmex export.
+        number_format($result->tax * -1, 2, '.', '') // Credit values are negative in Collmex export.
+    );
+}
+
+// Write output.
+if (($handle = fopen($output_filename, 'w')) !== FALSE) {
+    foreach ($output as $fields) {
+        fputcsv($handle, $fields);
+    }
+    fclose($handle);
+} else {
+    die("Failed to open output file '$output_filename' for writing.\n");
+}
+
+// Echo total tax amount.
+$total_tax = array_reduce($results, function($carry, $item) {
+    return $carry + $item->tax;
+}, 0);
+echo "Total tax: " . number_format(-$total_tax, 2) . PHP_EOL;
